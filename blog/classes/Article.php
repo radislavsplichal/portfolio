ec69f166bbb21ccd36b86fd7dbebac372fd5c07b
radislavsplichal <radislavsplichal@gmail.com>
2017-03-30 18:13:18 +0200

    re merger

--- blog/classes/Article.php
+++ blog/classes/Article.php
@@ -32,31 +32,22 @@ public function displayArticle () {
         <div class="col-md-10">
         <div class="panel panel-default">
         <div class="panel-heading"><b>'.$this->title.'</b>
-        <div class="btn-group" role="group" aria-label="...">
-        <form method="POST" action="editArticle.php">
+        <div class="administration">
+        <form method="POST" action="deleteArticle.php">
+
           <input type="hidden" name="id" value='.$this->id.'></input>
-          <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-        </form>
-        <input type="hidden" name="id" value='.$this->id.'></input>
           <span onClick="this.parentNode.submit();" type="submit" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
         </form>
+        <form method="POST" action="editArticle.php">
+          <input type="hidden" name="id" value='.$this->id.'></input>
+          <span onClick="this.parentNode.submit();" class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+        </form>
+
         </div>
         </br>
         Author: <i>'.$this->author.'</i></br>
         Created: '.$this->dateCreated.'
-          <div class="administration">
-          <form method="POST" action="deleteArticle.php">
-<<<<<<< HEAD
-            <input type="hidden" name="id" value='.$this->id.'></input>
-            <span onClick="this.parentNode.submit();" type="submit" class="glyphicon glyphicon-remove" aria-hidden="true"></span>
-          </form>
-          <form method="POST" action="editArticle.php">
-            <input type="hidden" name="id" value='.$this->id.'></input>
-            <span onClick="this.parentNode.submit();" class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-          </form>
-=======
->>>>>>> origin/master
-          </div>
+
           </div>
         <div class="panel-body">
         '.$this->article.'
@@ -96,8 +87,5 @@ if ($conn->query($sql) === TRUE){
 
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
 ?>
