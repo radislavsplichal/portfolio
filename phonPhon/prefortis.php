851436ef1bfc3b8fe1cdc74c27c91323a695bb65
radislavsplichal <radislavsplichal@MacBook-Pro-3.local>
2017-04-12 22:54:48 +0200

    pre fortis clipping

---
+++ phonPhon/prefortis.php
@@ -0,0 +1,24 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Portfolio</title>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <!-- jQuery library -->
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+</head>
+
+<body>
+  <div class="container">
+    
+  </div>
+</body>
+</html>
