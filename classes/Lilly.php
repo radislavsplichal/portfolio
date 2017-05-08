08f9b424663596cc6b970ebe2ae3b649991243fe
radislavsplichal <radislavsplichal@gmail.com>
2017-05-08 19:23:15 +0200

    LOL

--- classes/Lilly.php
+++ classes/Lilly.php
@@ -1,6 +1,12 @@
 <?php
 //this is an experimental approach to unify the site management processes
-//Lilly can do stuff and all it needs is just a call.
+
+//Lilly can do stuff and all she needs is just a call.
+
+//She is not an object. She is a woman. She might behave the same but it is a
+//great insult to think of her as an object.
+
+//to instatiate her just create an bot-> new Lilly; and use one of her trics
 
 //To be added and removed from all the classes
 //include ('databaseConnection.php');
@@ -8,6 +14,10 @@
 class Lilly {
 
 	//private $name = Lilly;
+	public function makeSandwich($type) {
+		echo "go F* yourself :D";
+
+	}
 
 	public function saveObject ($type,$arguments,$values,$conn) {
 
